9892ed398ca0c6cfcb909cb613efaac83442e060
Martin <martin@void.site>
2015-05-14 14:39:44 +0200

    Integrate unit tests.

--- Makefile
+++ Makefile
@@ -6,7 +6,11 @@ test_suite_console.c \
 test_suite_display.c \
 test_suite_screen.c \
 test_suite_screen_action.c \
-stack.c console.c display.c screen.c screen_text.c screen_action.c
+test_suite_value_to_str.c \
+test_suite_screen_value.c \
+test_suite_screen_option.c \
+stack.c console.c display.c screen.c screen_text.c screen_action.c  screen_option.c \
+screen_value.c utils.c
 OBJS = $(SRCS:.c=.o)
 INSTALL_PATH=bin
 
--- test.c
+++ test.c
@@ -5,6 +5,10 @@ void display_test_suite();
 void screen_test_suite();
 void screen_action_test_suite();
 
+void value_to_str_test_suite();
+void screen_value_test_suit();
+void screen_option_test_suite();
+
 int main() {
     utest_init();
 
@@ -12,7 +16,11 @@ int main() {
     display_test_suite();
     screen_test_suite();
     screen_action_test_suite();
-    
+
+    value_to_str_test_suite();
+    screen_value_test_suit();
+    screen_option_test_suite();
+
     TEST_SUMMARY();
     return 0;
 }
